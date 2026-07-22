8c7c45bf2d034ec5bae3e63b623ade6e3dd62913
Duncan Eddy <duncan.eddy@gmail.com>
2026-07-22 08:23:13 -0700

    Add RA/Dec to azimuth-elevation test coverage

--- tests/test_radec.py
+++ tests/test_radec.py
@@ -68,3 +68,75 @@ def test_bad_vector_length_errors():
 def test_state_to_azel_unsupported():
     res = convert_radec([1.0] * 6, "RADEC", "AZEL")
     assert "error" in res
+
+
+SITE = [-122.4, 37.8, 100.0]
+EPOCH = "2024-01-01T00:00:00Z"
+
+
+def test_radec_to_azel_matches_brahe():
+    radec = [45.0, 20.0, 1.0e9]
+    res = convert_radec(radec, "RADEC", "AZEL", site=SITE, epoch=EPOCH)
+    assert "error" not in res
+    expected = brahe.position_radec_to_azel(
+        np.array(radec), np.array(SITE), brahe.Epoch(EPOCH),
+        brahe.AngleFormat.DEGREES,
+    )
+    assert np.allclose(res["output"]["vector"], expected)
+    assert set(res["output"]["components"]) == {"az", "el", "range_m"}
+
+
+def test_radec_azel_roundtrip():
+    radec = [45.0, 20.0, 1.0e9]
+    fwd = convert_radec(radec, "RADEC", "AZEL", site=SITE, epoch=EPOCH)
+    back = convert_radec(
+        fwd["output"]["vector"], "AZEL", "RADEC", site=SITE, epoch=EPOCH
+    )
+    assert np.allclose(back["output"]["vector"], radec, rtol=1e-8)
+
+
+def test_azel_requires_site():
+    res = convert_radec([45.0, 20.0, 1.0e9], "RADEC", "AZEL", epoch=EPOCH)
+    assert "error" in res
+    assert "site" in res["error"]
+
+
+def test_azel_requires_epoch():
+    res = convert_radec([45.0, 20.0, 1.0e9], "RADEC", "AZEL", site=SITE)
+    assert "error" in res
+    assert "epoch" in res["error"]
+
+
+def test_azel_bad_site_length():
+    res = convert_radec(
+        [45.0, 20.0, 1.0e9], "RADEC", "AZEL", site=[1.0, 2.0], epoch=EPOCH
+    )
+    assert "error" in res
+
+
+def test_azel_invalid_site_type():
+    res = convert_radec(
+        [45.0, 20.0, 1.0e9], "RADEC", "AZEL",
+        site=SITE, site_type="lunar", epoch=EPOCH,
+    )
+    assert "error" in res
+    assert "site_type" in res["error"]
+
+
+def test_azel_ecef_site_matches_geodetic_site():
+    radec = [45.0, 20.0, 1.0e9]
+    ecef = brahe.position_geodetic_to_ecef(
+        np.array(SITE), brahe.AngleFormat.DEGREES
+    ).tolist()
+    a = convert_radec(radec, "RADEC", "AZEL", site=SITE, epoch=EPOCH)
+    b = convert_radec(
+        radec, "RADEC", "AZEL", site=ecef, site_type="ecef", epoch=EPOCH
+    )
+    assert np.allclose(a["output"]["vector"], b["output"]["vector"], rtol=1e-6)
+
+
+def test_azel_invalid_epoch():
+    res = convert_radec(
+        [45.0, 20.0, 1.0e9], "RADEC", "AZEL", site=SITE, epoch="not-a-date"
+    )
+    assert "error" in res
